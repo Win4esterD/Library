b3a48c4b1324795a65e98f6458918695a3ce082e
Pavel Didenko <patriot2006_06@mail.ru>
2023-07-24 17:28:13 +0300

    feat: implements adaptive and responsive designs for resolutions from 768px until 320px

--- library/src/components/contacts/Contacts.jsx
+++ library/src/components/contacts/Contacts.jsx
@@ -21,7 +21,9 @@ const Contacts = () => {
               <p className="contacts__adress-and-number">Library Director: <a className="contacts__link contacts__director" href="mailto:AmandaHirst@gmail.com">Amanda Hirst</a></p>
             </div>
           </div>
-          <a className="contacts__map" href="https://www.google.com/maps/place/286+Cadman+Plaza+W,+Brooklyn,+NY+11201,+USA/@40.6952685,-73.9916289,19z/data=!4m6!3m5!1s0x89c25a49b649b8c3:0x6342a2291cea2e!8m2!3d40.6956458!4d-73.9912976!16s%2Fg%2F11s2lgz8ns?entry=ttu"><img src="./assets/img/other/map.png" alt="map" /></a>
+          <a className="contacts__map" href="https://www.google.com/maps/place/286+Cadman+Plaza+W,+Brooklyn,+NY+11201,+USA/@40.6952685,-73.9916289,19z/data=!4m6!3m5!1s0x89c25a49b649b8c3:0x6342a2291cea2e!8m2!3d40.6956458!4d-73.9912976!16s%2Fg%2F11s2lgz8ns?entry=ttu">
+            <img className="contacts__img" src="./assets/img/other/map.png" alt="map" />
+            </a>
         </div>
       </div>
     </section>
