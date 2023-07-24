942b55960b178bb8a7bf6030d2159ab156e2c68a
Pavel Didenko <patriot2006_06@mail.ru>
2023-07-24 23:56:06 +0300

    feat: implement functionality for the caorusel, to make left and right buttons switch slides

--- library/src/components/about/carousel/Caorusel.jsx
+++ library/src/components/about/carousel/Caorusel.jsx
@@ -1,10 +1,34 @@
 import './carousel.scss';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const Carousel = () => {
   const [value, setValue] = useState('first');
   const idName = 'toggler_active';
   const [shift, setShift] = useState({transform: 'translateX(0%)'});
+
+  const firstToggler = useRef(0);
+  const secondToggler = useRef(1);
+  const thirdToggler = useRef(2);
+
+  function handleRightArrow(){
+    if(value==='first'){
+      secondToggler.current.click();
+    }else if(value==='second'){
+      thirdToggler.current.click();
+    }else{
+      firstToggler.current.click();
+    }
+  }
+
+  function handleLeftArrow(){
+    if(value==='third'){
+      secondToggler.current.click();
+    }else if(value==='second'){
+      firstToggler.current.click();
+    }else{
+      thirdToggler.current.click();
+    }
+  }
 
     return(
       <>
@@ -16,11 +40,21 @@ const Carousel = () => {
           <img className="carousel__img" style={shift} src="./assets/img/caorusel/caorusel5.png" alt="slide 5" />
         </div>
         <div className="arrows-wrapper">
-          <img className="carousel__arrow" src="./assets/img/icons/arrow-left.svg" alt="arrow-left" />
-          <img className="carousel__arrow" src="./assets/img/icons/arrow-right.svg" alt="arrow-right" />
+          <img className="carousel__arrow"
+            onClick={handleLeftArrow}
+            src="./assets/img/icons/arrow-left.svg"
+            alt="arrow-left" 
+            />
+          <img className="carousel__arrow"
+            onClick={handleRightArrow}
+            src="./assets/img/icons/arrow-right.svg"
+            alt="arrow-right" 
+            />
         </div>
         <div className="togglers">
-          <div className="togglers__external" onClick={
+          <div className="togglers__external" 
+            ref={firstToggler} 
+            onClick={
             () => {
               setValue('first');
               setShift({transform: 'translateX(0%)'});
@@ -32,7 +66,9 @@ const Carousel = () => {
             
             </div>
           </div>
-          <div className="togglers__external" onClick={
+          <div className="togglers__external" 
+            ref={secondToggler} 
+            onClick={
             () => {
               setValue('second');
               setShift({transform: 'translateX(-107%)'});
@@ -43,7 +79,9 @@ const Carousel = () => {
             
             </div>
           </div>
-          <div className="togglers__external" onClick={
+          <div className="togglers__external" 
+            ref={thirdToggler}
+            onClick={
             () => {
               setValue('third');
               setShift({transform: 'translateX(-215%)'});
